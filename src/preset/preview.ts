348cbeed4cceb0938bf6a1e49fee240568e93124
Muhammad Ahsan Ayaz <=>
2022-08-26 11:00:08 +0200

    fix(globalDecorators): remove global decorators

--- src/preset/preview.ts
+++ src/preset/preview.ts
@@ -8,7 +8,8 @@
  *
  * https://storybook.js.org/docs/react/writing-stories/decorators#gatsby-focus-wrapper
  */
+import { DecoratorFunction } from "@storybook/addons";
 import { withGlobals } from "../withGlobals";
 import { withRoundTrip } from "../withRoundTrip";
 
-export const decorators = [withGlobals, withRoundTrip];
+export const decorators: DecoratorFunction[] = [];
